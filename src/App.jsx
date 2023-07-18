a57b4ea90e0df4197ab084b9cf895bd63cb64e55
Inder <inderpreet171222@gmail.com>
2023-07-18 09:40:08 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from "react";
-
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Projects from "./Components/Projects";
 
 function App() {
+  // State to store the current route
+  const [currentRoute, setCurrentRoute] = useState("/");
+
+  // Use useEffect to update the current route in localStorage whenever it changes
+  useEffect(() => {
+    localStorage.setItem("currentRoute", currentRoute);
+  }, [currentRoute]);
+
+  // Use useEffect to get the initial route from localStorage when the app starts
+  useEffect(() => {
+    const initialRoute = localStorage.getItem("currentRoute");
+    if (initialRoute) {
+      setCurrentRoute(initialRoute);
+    }
+  }, []);
+
   return (
     <Router>
       <div className="absolute bg-gray-900 text-white">
@@ -17,10 +31,13 @@ function App() {
         </div>
 
         <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/about" element={<About />}></Route>
-          <Route exact path="/contact" element={<Contact />}></Route>
-          <Route exact path="/projects" element={<Projects />}></Route>
+          <Route
+            path="/"
+            element={<Home setCurrentRoute={setCurrentRoute} />}
+          />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
       </div>
     </Router>
